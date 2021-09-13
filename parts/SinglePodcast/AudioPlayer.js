253c390e6a7ddb16259c8dc4b51c1e270fc37b38
Aziz Bektemirov <bektemirovaziz64@yandex.com>
2021-09-13 09:59:07 +0500

    finish the player

--- parts/SinglePodcast/AudioPlayer.js
+++ parts/SinglePodcast/AudioPlayer.js
@@ -3,28 +3,75 @@ import styles from './AudioPlayer.module.css'
 import { MdVolumeUp, MdReplay10, MdForward30, MdPause, MdPlayArrow } from 'react-icons/md'
 
 const AudioPlayer = (props) => {
-
+    const classes = `${styles.podcastPlayer}`
     const audioTrackRef = useRef();
-    const audioTrackCurrent = audioTrackRef.current
+    const audioTrackProgressWrapper = useRef();
     const [isPlay, setIsPlay] = useState(false);
+    const [podcastDuration, setPodcastDuration] = useState("00:00");
+    const [podcastCurrentTime, setPodcastCurrentTime] = useState("00:00");
+    const [barWidth, setBarWidth] = useState("0");
 
+    const audioTimeConverted = (time) => {
+        const tempMinutes = Math.floor(time / 60);
+        const tempSeconds = Math.floor(time % 60);
+        const minutes = tempMinutes < 10 ? `0${tempMinutes}` : `${tempMinutes}`;
+        const seconds = tempSeconds < 10 ? `0${tempSeconds}` : `${tempSeconds}`;
+        return `${minutes}:${seconds}`;
+    };
+
+    function onReplayButtonClickHandler (e) {
+        e.preventDefault();
+        audioTrackRef.current.currentTime -=15
+    }
+    function onForwardButtonClickHandler (e) {
+        e.preventDefault();
+        audioTrackRef.current.currentTime +=30
+    }
+
+    function onProgressBarClickHanlder(e) {
+        audioTrackRef.current.currentTime = e.nativeEvent.offsetX / audioTrackProgressWrapper.current.offsetWidth * audioTrackRef.current.duration;
+    }
 
     function onPlayPauseClickHandler(e) {
         e.preventDefault();
+        const prevPlayState = isPlay;
         setIsPlay(playState => !playState);
-
-        if (isPlay) {
-            audioTrackCurrent.pause();
+        if (prevPlayState) {
+            audioTrackRef.current.pause();
         } else {
-            audioTrackCurrent.pause();
+            audioTrackRef.current.play();
         }
+    }
+
+    function onAudioLoadedMetadata() {
+        const trackDuration = audioTrackRef.current.duration;
+        setPodcastDuration(audioTimeConverted(trackDuration));
+    };
+
+    function onCurrentTimeChangeHandler() {
+        const currentTime = audioTimeConverted(audioTrackRef.current.currentTime);
+        setPodcastCurrentTime(currentTime);
+        const audioBarWidth = ((audioTrackRef.current.currentTime * 100) / audioTrackRef.current.duration).toFixed(2);
+        setBarWidth(audioBarWidth);
+    }
+
+    function onAudioEndHandler () {
+        setIsPlay(false);
     }
 
     return (
         <div className={styles.podcastPlayer}>
-            <audio ref={audioTrackRef} src={props.audio}></audio>
-            <div className={styles.podcastPlayer__progressBar}>
-                <div className={styles.podcastPlayer__progressBarFill}></div>
+            <audio onEnded={onAudioEndHandler} onTimeUpdate={onCurrentTimeChangeHandler} onLoadedMetadata={onAudioLoadedMetadata} ref={audioTrackRef} src={props.audio}></audio>
+            <div
+                onClick={onProgressBarClickHanlder}
+                ref={audioTrackProgressWrapper}
+                className={styles.podcastPlayer__progressBar}>
+                <div
+                    onClick={onProgressBarClickHanlder}
+                    style={{
+                        width: `${barWidth}%`
+                    }}
+                    className={styles.podcastPlayer__progressBarFill}></div>
             </div>
             <div className={styles.podcastPlayer__controls}>
                 <div className={styles.podcastPlayer__volumeAndTime}>
@@ -32,19 +79,19 @@ const AudioPlayer = (props) => {
                         <MdVolumeUp />
                     </button>
                     <div className={styles.podcastPlayer__time}>
-                        <p>0:15</p>
+                        <p>{podcastCurrentTime}</p>
                         <span>/</span>
-                        <p>45:10</p>
+                        <p>{podcastDuration}</p>
                     </div>
                 </div>
                 <div className={styles.podcastPlayer__playControls}>
-                    <button className={styles.podcastPlayer__back}>
+                    <button onClick={onReplayButtonClickHandler} className={styles.podcastPlayer__back}>
                         <MdReplay10 />
                     </button>
                     <button onClick={onPlayPauseClickHandler} className={styles.podcastPlayer__playPause}>
                         {isPlay ? <MdPause /> : <MdPlayArrow />}
                     </button>
-                    <button className={styles.podcastPlayer__next}>
+                    <button onClick={onForwardButtonClickHandler} className={styles.podcastPlayer__next}>
                         <MdForward30 />
                     </button>
                 </div>

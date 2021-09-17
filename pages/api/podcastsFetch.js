96f78fe8a33ed995208f7908222e0991769d4842
Aziz Bektemirov <bektemirovaziz64@yandex.com>
2021-09-17 22:10:42 +0500

    fix localhost to vercel link

--- pages/api/podcastsFetch.js
+++ pages/api/podcastsFetch.js
@@ -1,6 +1,6 @@
 const podcastFetch = (req, res) => {
-    // fetch("https://webmag-podcast.vercel.app/podcastData.json")
-    fetch("http://localhost:3000/podcastData.json")
+    fetch("https://webmag-podcast.vercel.app/podcastData.json")
+    // fetch("http://localhost:3000/podcastData.json")
     .then(r => r.json())
         .then(data =>
             res.status(200).json({
